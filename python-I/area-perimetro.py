43beb0b701a45b3aed81c44abe4c77816a79907b
Gabriel Godoy <gabrielgdy090@users.noreply.github.com>
2024-01-12 21:45:14 -0300

    Criei Lista de exercícios 1

---
+++ python-I/area-perimetro.py
@@ -0,0 +1,8 @@
+﻿valorLado = int(input("Digite a medida do lado do quadrado"))
+
+perimetro = valorLado*4
+area = valorLado*valorLado
+
+
+print("O perimetro do quadrado é igual a",perimetro)
+print(" A area do quadrado é igual a", area)
---
+++ python-I/media-aritimetica.py
@@ -0,0 +1,10 @@
+﻿valorA=int(input("Digite um valor"))
+valorB=int(input("Digite um valor"))
+valorC=int(input("Digite um valor"))
+valorD=int(input("Digite um valor"))
+valorE=int(input("Digite um valor"))
+
+
+mediaAritimetica = valorA+valorB+valorC+valorD+valorE/5
+
+print("A média aritmetica é igual a:",mediaAritimetica)
